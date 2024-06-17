42d91beee370f1892dde49e1d962630f2c466c77
aneesh331 <aneeshbonthala15@gmail.com>
2024-06-16 23:26:04 -0700

    organized codebase; initiate babadook trivia

---
+++ src/Start.js
@@ -0,0 +1,13 @@
+import './styles/Start.css';
+
+function Start() {
+  return (
+    <div className="background">
+      <div className="hi">
+        hi
+      </div>
+    </div>
+  );
+}
+
+export default Start;
